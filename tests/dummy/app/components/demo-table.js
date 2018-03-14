8f62e695522f1fbf5801a9b3f5595ccfe70af6f1
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-03-14 18:51:50 +0000

    Working demo except target routes

--- tests/dummy/app/components/demo-table.js
+++ tests/dummy/app/components/demo-table.js
@@ -1,27 +1,36 @@
 import { computed } from '@ember/object';
+import Component from '@ember/component';
 import Table from 'ember-light-table';
 
-export default Ember.Component.extend({
+// note: template MUST be in /app or the dummy crashes
+
+export default Component.extend({
   model: null,
 
   columns: computed(function() {
     return [{
       label: 'Title',
       valuePath: 'title',
+      sortable: false,
+      // changing cellType to componentType the td no longer receives the data (unexpectedly according to docs).
+      // difference explained here https://github.com/offirgolan/ember-light-table/issues/536:
+      // componentType is nested within a TD, cellType is the component that generates the TD
       cellType: 'link',
       extra: {
-        modelName: 'project'
+        route: 'rentals' // as found in the Ember Guide Tutorial
       }
     }, {
       label: 'Owner',
-      valuePath: 'owner'
+      valuePath: 'owner',
+      sortable: false
     }, {
       label: 'City',
-      valuePath: 'city'
+      valuePath: 'city',
+      sortable: false
     }];
   }),
 
   table: computed('model', function() {
-   return new Table(this.get('columns'), this.get('model'));
+    return new Table(this.get('columns'), this.get('model'));
   })
 });
