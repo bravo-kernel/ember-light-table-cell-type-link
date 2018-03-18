b5bbc3713d4bb189d8d8ec400510991478a4967f
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-03-18 14:29:51 +0000

    Add more test data

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -7,30 +7,50 @@ export default Route.extend({
         id: 1,
         type: 'user',
         attributes: {
-          firstName: 'Scarlett',
-          lastName: 'Johansson',
-          city: 'New York',
-          website: 'http://www.imdb.com/name/nm0424060/'
+          firstName: 'Megan',
+          lastName: 'Fox',
+          city: 'Rockwood',
+          website: 'https://www.imdb.com/name/nm1083271/'
         },
         relationships: {}
       }, {
         id: 2,
         type: 'user',
         attributes: {
-          firstName: 'Megan',
-          lastName: 'Fox',
-          city: 'Rockwood',
-          website: 'http://www.imdb.com/name/nm1083271/'
+          firstName: 'Michelle',
+          lastName: 'Yeoh',
+          city: 'Ipoh',
+          website: 'https://www.imdb.com/name/nm0000706/'
         },
         relationships: {}
       }, {
         id: 3,
         type: 'user',
         attributes: {
+          firstName: 'Salma',
+          lastName: 'Hayek',
+          city: 'Coatzacoalcos',
+          website: 'https://www.imdb.com/name/nm0000161/'
+        },
+        relationships: {}
+      }, {
+        id: 4,
+        type: 'user',
+        attributes: {
+          firstName: 'Scarlett',
+          lastName: 'Johansson',
+          city: 'New York',
+          website: 'https://www.imdb.com/name/nm0424060/'
+        },
+        relationships: {}
+      }, {
+        id: 5,
+        type: 'user',
+        attributes: {
           firstName: 'Zoe',
           lastName: 'Zaldana',
           city: 'Passaic',
-          website: 'http://www.imdb.com/name/nm0757855/'
+          website: 'https://www.imdb.com/name/nm0757855/'
         },
         relationships: {}
       }]
