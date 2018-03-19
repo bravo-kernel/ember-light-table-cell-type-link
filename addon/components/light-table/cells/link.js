72cfd9437289cc4b788ea6c1581594941e1d0aab
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-03-19 18:51:33 +0000

    Adds support for external links

--- addon/components/light-table/cells/link.js
+++ addon/components/light-table/cells/link.js
@@ -1,5 +1,3 @@
-import { assert } from '@ember/debug';
-import { isEmpty } from '@ember/utils';
 import BaseCell from 'ember-light-table/components/cells/base';
 import layout from 'ember-light-table-cell-type-link/templates/components/light-table/cells/link'
 
@@ -8,9 +6,6 @@ import layout from 'ember-light-table-cell-type-link/templates/components/light-
  * @extends LightTable.BaseCell
  */
 export default class extends BaseCell.extend({ layout }) {
-  willRender() {
-    if (isEmpty(this.column.extra.route)) {
-      assert ("ember-light-table-cell-type-link requires specifying the 'extra.route' attribute in your ELT column definition");
-    }
-  }
+
 }
+
